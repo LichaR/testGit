1957392e8a242cc3bf1d7f322b986631bbab93cd
LichaR <chano_licha@hotmail.com>
2014-11-23 00:48:59 -0300

    Modificacion

--- testGitHub/Form1.Designer.cs
+++ testGitHub/Form1.Designer.cs
@@ -29,9 +29,7 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-
             this.domainUpDown1 = new System.Windows.Forms.DomainUpDown();
-
             this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
@@ -44,7 +42,6 @@
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
-
             // domainUpDown1
             // 
             this.domainUpDown1.Location = new System.Drawing.Point(48, 182);
@@ -55,18 +52,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(25, 340);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(128, 104);
-            this.button2.TabIndex = 2;
-
-            // button2
-            // 
             this.button2.Location = new System.Drawing.Point(264, 155);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
-
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
             // 
@@ -74,14 +63,9 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-
-            this.ClientSize = new System.Drawing.Size(421, 499);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(416, 262);
             this.Controls.Add(this.domainUpDown1);
-
-            this.ClientSize = new System.Drawing.Size(421, 262);
             this.Controls.Add(this.button2);
-
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
